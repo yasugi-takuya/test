0095528fd5b7f83d317c85615664412fd8a36f57
yasugi_takuya <kd1376223@st.kobedenshi.ac.jp>
2025-06-12 23:23:50 +0900

    意味表示機能の試作

--- public/game.js
+++ public/game.js
@@ -2,10 +2,11 @@ const title = document.getElementById('title');  // タイトルを表示するh
 const yojijukugo = document.getElementById('yojijukugo');  // 四字熟語と読み仮名を表示するh2タグ
 const kanji = document.querySelectorAll('.kanji');  // 四字熟語の各漢字を表示するrubyタグのリスト
 const yojijukugo_meaning = document.getElementById('yojijukugo_meaning');  // 四字熟語の意味を表示するpタグ
+const next_btn = document.getElementById('next_btn');  // 次の四字熟語を表示するボタン
 
 const url = 'yojijukugo (1).json';  // 四字熟語が書かれたJSONファイルのurl(相対パス)
 let counter = 0;  // 四字熟語のカウンタ
-let = yojijukugo_json;  // 四字熟語のJSONデータ
+let yojijukugo_json;  // 四字熟語のJSONデータ
 
 // JSONファイルのデータの取得
 fetch(url)
@@ -18,7 +19,7 @@ fetch(url)
     yojijukugo_display(counter);  // 最初の四字熟語の表示
 })
 .catch((e) => {  // 上記の処理中にエラーが発生したら
-    window.alert(e);  //エラーをキャッチし表示     
+    window.alert(e);  //エラーをキャッチし表示
 })
 
 // 四字熟語を表示する関数
@@ -26,9 +27,20 @@ const yojijukugo_display = (counter) => {
     // 四字熟語の表示
     yojijukugo.innerHTML = `${yojijukugo_json[counter].熟語}<rt>${yojijukugo_json[counter].読み方}</rt>`;
     // 四字熟語の各漢字を表示するループ
-    for (let i = 0; i < yojijukugo_json.length; i++)
+    for (let i = 0; i < kanji.length; i++)
     {
         kanji[i].innerHTML = `${yojijukugo_json[counter].構成漢字[i][0]}<rt>${yojijukugo_json[counter].構成漢字[i][1]}</rt>`;
     }
+    // 四字熟語の意味の表示
     yojijukugo_meaning.textContent = yojijukugo_json[counter].意味;
 }
+
+// ボタンのクリックイベント
+next_btn.addEventListener('click', () => {
+    counter += 1;  // 四字熟語のカウンタをカウントアップ
+    if (counter >= yojijukugo_json.length)  // カウンタが四字熟語の個数を超えたら
+    {
+        counter = 0;  // 最初の四字熟語に戻る
+    }
+    yojijukugo_display(counter);  // 四字熟語の表示
+})
